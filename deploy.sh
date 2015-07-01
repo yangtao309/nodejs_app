4d08543acfdfc9fb425f00360a6decd53f31bc0e
tao.yang <tao.yang@shuyun.com>
2015-07-01 13:11:09 +0800

    issue:none

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,12 @@ else
    marathon=${MARATHON_PORT_8080_TCP_ADDR}
 fi
 
-curl -X DELETE -H "Content-Type: application/json" http://${marathon}:8080/v2/apps/app 
+curl -X DELETE -H "Content-Type: application/json" http://${marathon}:8090/v2/apps/app 
 sleep 1
-#curl -X POST -H "Content-Type: application/json" http://localhost:8080/v2/apps -d@app_marathon.json
+#curl -X POST -H "Content-Type: application/json" http://localhost:8090/v2/apps -d@app_marathon.json
 
 cp -f app_marathon.json app_marathon.json.tmp
 sed -i "s/latest/${version}/g" app_marathon.json.tmp
 
 
-curl -X POST -H "Content-Type: application/json" http://${marathon}:8080/v2/apps -d@app_marathon.json.tmp
+curl -X POST -H "Content-Type: application/json" http://${marathon}:8090/v2/apps -d@app_marathon.json.tmp
