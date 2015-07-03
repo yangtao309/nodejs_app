334f59bf27095dc128ff7a7ba7832e9560e70b8f
yangtao309 <yangtao309@gmail.com>
2015-07-03 04:12:17 +0000

    issue:none

---
+++ docker-compose.sh
@@ -0,0 +1,3 @@
+
+sudo cp ./docker-compose /usr/local/bin/docker-compose
+sudo chmod 755 /usr/local/bin/docker-compose
---
+++ fig.sh
@@ -0,0 +1,4 @@
+
+
+sudo cp ./fig /usr/local/bin/fig 
+sudo chmod 755 /usr/local/bin/fig
