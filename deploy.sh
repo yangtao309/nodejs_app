5f8fd061175ba220cb22a1372b9bcf12b0f887f0
yangtao309 <yangtao309@gmail.com>
2015-07-07 12:59:44 +0000

    issue:none

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ else
    marathon="192.168.59.103"
 fi
 
-curl -X DELETE -H "Content-Type: application/json" http://${marathon}:8090/v2/apps/app 
+curl -X DELETE -H "Content-Type: application/json" http://${marathon}:8080/v2/apps/app 
 sleep 1
 #curl -X POST -H "Content-Type: application/json" http://localhost:8090/v2/apps -d@app_marathon.json
 
@@ -16,4 +16,4 @@ cp -f app_marathon.json app_marathon.json.tmp
 sed -i "s/latest/${version}/g" app_marathon.json.tmp
 
 
-curl -X POST -H "Content-Type: application/json" http://${marathon}:8090/v2/apps -d@app_marathon.json.tmp
+curl -X POST -H "Content-Type: application/json" http://${marathon}:8080/v2/apps -d@app_marathon.json.tmp
